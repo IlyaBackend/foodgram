f00ba2d423fca28d2844bb7ba9dc71fb880170b4
Илья Илья <gamemaster438@yandex.ru>
2025-10-14 02:57:15 +0300

    csrf admin

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -14,6 +14,7 @@ USE_SQLITE = os.environ.get('USE_SQLITE') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
